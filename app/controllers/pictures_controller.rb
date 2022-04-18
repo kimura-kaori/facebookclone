d1b6a92804b77a786f95db227b47937749347fef
kaori kimura <tkhy_1125@icloud.com>
2022-04-18 11:04:07 +0900

    一覧表示と詳細画面の実装

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,5 +1,6 @@
 class PicturesController < ApplicationController
   def index
+    @pictures = Picture.all
   end
 
   def new
@@ -9,15 +10,22 @@ class PicturesController < ApplicationController
   def create
     @picture = current_user.pictures.build(image: params[:picture][:image], content: params[:picture][:content])
     if @picture.save
-      redirect_to blogs_path, notice: "投稿しました！"
+      redirect_to pictures_path, notice: "投稿しました！"
     else
       render :new
     end
   end
+
+  def show
+    @picture = Picture.find(params[:id])
+  end
+
+  def edit
+  end
+
+  private
   #
-  # def show
-  # end
-  #
-  # def edit
+  # def picture_params
+  #   params.require(:picture).permit(:image, :content)
   # end
 end
