d55b0e079b9f9f0584b7f7ab0eb787fbec748e2e
kaori kimura <tkhy_1125@icloud.com>
2022-04-18 10:36:09 +0900

    createアクション設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,9 +2,22 @@ class PicturesController < ApplicationController
   def index
   end
 
-  def show
+  def new
+    @picture = Picture.new
   end
 
-  def edit
+  def create
+    @picture = current_user.pictures.build(image: params[:picture][:image], content: params[:picture][:content])
+    if @picture.save
+      redirect_to blogs_path, notice: "投稿しました！"
+    else
+      render :new
+    end
   end
+  #
+  # def show
+  # end
+  #
+  # def edit
+  # end
 end
